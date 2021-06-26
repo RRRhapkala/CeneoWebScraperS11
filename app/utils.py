9911be35bb1f7e16a6e56e9fb897ec6cd934f3db
RRRhapkala <grixagrodnoblr@gmail.com>
2021-06-26 12:17:48 +0200

    26 Final Project

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,12 @@
-def get_component(tag_subtree, selector, attribute=None, return_list=False):
+
+def extract_element(dom_tree, selector, attribute=None):
     try:
         if attribute:
-            return tag_subtree.select(selector).pop(0)[attribute].strip()
-        if return_list:
-            return [item.get_text().strip() for item in tag_subtree.select(selector)]
-        return tag_subtree.select(selector).pop(0).get_text().strip()
+            if isinstance(attribute, str):
+                return dom_tree.select(selector).pop(0)[attribute].strip()
+            else:
+                return [x.get_text().strip() for x in dom_tree.select(selector)]
+        else:
+            return dom_tree.select(selector).pop(0).get_text().strip()
     except IndexError:
         return None
